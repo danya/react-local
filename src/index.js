58979dadad8938f207849704ef0358527b10dd7d
Daniil Poroshin <daniilporoshin@gmail.com>
2018-10-21 01:37:33 +0300

    0.2.0

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const t = require('@babel/types')
 
 function plugin() {
   return {
-    pre(state) {
+    pre() {
       this.history = {}
     },
 
@@ -15,6 +15,7 @@ function plugin() {
           return
         }
 
+        // Plugin options
         const {
           pragma = 'createElement',
           pragmaFrag = 'Fragment',
@@ -22,48 +23,54 @@ function plugin() {
           alwaysFragment = false,
           declaration = 'const'
         } = state.opts
+        const pragmas = { createElement: pragma, Fragment: pragmaFrag }
 
-        const names = path.node.specifiers
+        // Import specifiers
+        const names = []
+        const locals = []
+
+        path.node.specifiers
           .filter(s => s.type === 'ImportSpecifier')
-          .map(s => s.imported.name)
+          .forEach(({ imported, local }) => {
+            names.push(imported.name)
+            locals.push(imported.name !== local.name ? local.name : null)
+          })
 
-        if (names.indexOf('createElement') === -1 && alwaysCreateElement) {
+        if (alwaysCreateElement && names.indexOf('createElement') < 0) {
           names.push('createElement')
         }
 
-        if (names.indexOf('Fragment') === -1 && alwaysFragment) {
+        if (alwaysFragment && names.indexOf('Fragment') < 0) {
           names.push('Fragment')
         }
 
-        const properties = names.map(name => {
-          const variable =
-            name === 'createElement'
-              ? pragma
-              : name === 'Fragment'
-                ? pragmaFrag
-                : name
-
-          return t.objectProperty(t.identifier(name), t.identifier(variable))
-        })
-
-        // Import React as default:
-        // `import React from 'react'`
-        const Import = t.importDeclaration(
-          [t.importDefaultSpecifier(t.identifier('React'))],
-          t.stringLiteral('react')
-        )
-
-        // Now get properties of React default export. For example:
-        // `const {createElement: createElement, Fragment: Fragment} = React`
-        const Variables = t.variableDeclaration(declaration, [
-          t.variableDeclarator(
-            t.objectPattern(properties),
-            t.identifier('React')
+        // Replace node if there're necessary imports
+        if (names.length > 0) {
+          // Import React as default:
+          // `import React from 'react'`
+          const Import = t.importDeclaration(
+            [t.importDefaultSpecifier(t.identifier('React'))],
+            t.stringLiteral('react')
           )
-        ])
 
-        // Then just replace old import declaration with the new one
-        path.replaceWithMultiple([Import, Variables])
+          // Extract properties for React default export as local vars:
+          // `const {createElement: createElement, Fragment: Fragment} = React`
+          const properties = names.map((name, i) =>
+            t.objectProperty(
+              t.identifier(name),
+              t.identifier(locals[i] || pragmas[name] || name)
+            )
+          )
+
+          const Variables = t.variableDeclaration(declaration, [
+            t.variableDeclarator(
+              t.objectPattern(properties),
+              t.identifier('React')
+            )
+          ])
+
+          path.replaceWithMultiple([Import, Variables])
+        }
 
         this.history[state.filename] = true
       }
