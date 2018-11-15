a346da91ee80e351d91b06d4f6473b67a2230744
Daniil Poroshin <daniilporoshin@gmail.com>
2018-11-16 00:05:00 +0300

    Update code for new features

--- src/index.js
+++ src/index.js
@@ -10,71 +10,130 @@ module.exports = () => ({
       if (this.history.has(state.filename)) return
       if (path.node.source.value !== 'react') return
 
-      const config = prepareConfig(state.opts)
-      const { imported, locals, identifier } = getDataFromImportNode(path.node)
+      const {
+        variable,
+        namedSpecifiers,
+        imported,
+        locals
+      } = getDataFromImportNode(path.node)
 
-      if (config.alwaysCreateElement && !imported.includes('createElement'))
-        imported.push('createElement')
-      if (config.alwaysFragment && !imported.includes('Fragment'))
-        imported.push('Fragment')
-
-      if (imported.length > 0) {
-        const variable = identifier || 'React'
-
-        // import statement
-        const importNode = t.ImportDeclaration(
-          [t.importDefaultSpecifier(t.identifier(variable))],
-          t.stringLiteral('react')
-        )
-
-        // extract object statement
-        const specified = locals.map((l, i) => (l !== imported[i] ? l : null))
-        const properties = imported.map((name, i) =>
-          t.objectProperty(
-            t.identifier(name),
-            t.identifier(specified[i] || config.pragmas[name] || name)
-          )
-        )
-        const declarationNode = t.variableDeclaration(config.declaration, [
-          t.variableDeclarator(
-            t.objectPattern(properties),
-            t.identifier(variable)
-          )
-        ])
-
-        path.replaceWithMultiple([importNode, declarationNode])
+      if (!imported.includes('createElement')) {
+        namedSpecifiers.push(emulateImportSpecifier('createElement'))
+      }
+      if (!imported.includes('Fragment')) {
+        namedSpecifiers.push(emulateImportSpecifier('Fragment'))
       }
 
+      const importIndex = path.parent.body.indexOf(path.node)
+      const ast = JSON.parse(JSON.stringify(path.parent))
+      ast.body[importIndex] = false
+
+      const amount = getAmountOfUse(locals, ast)
+
+      const imports = {}
+      const extract = {}
+
+      for (let i = 0, l = namedSpecifiers.length; i < l; i++) {
+        const importedName = namedSpecifiers[i].imported.name
+        const localName = namedSpecifiers[i].local.name
+        const count = amount[localName]
+
+        // TODO: Add error on wrong type
+        if (state.opts.extract === 'all' && count > 0) {
+          extract[importedName] = localName
+        } else {
+          if (count >= (state.opts.extract || 2)) {
+            extract[importedName] = localName
+          } else if (count > 0) {
+            imports[importedName] = localName
+          }
+        }
+      }
+
+      const identifier = t.identifier(variable || 'React')
+      const importNode = createImportNode(identifier, imports, 'react')
+      const extractNode =
+        Object.keys(extract).length > 0
+          ? createExtractNode(state.opts.declaration, identifier, extract)
+          : null
+
+      path.replaceWithMultiple([importNode, extractNode].filter(Boolean))
+
       this.history.add(state.filename)
+    },
+
+    MemberExpression(path) {
+      const {
+        object,
+        property: { name }
+      } = path.node
+
+      if (object.name !== 'React') return
+      if (name !== 'createElement' && name !== 'Fragment') return
+      if (t.isVariableDeclarator(path.parent)) return
+
+      const expression = t.expressionStatement(t.identifier(name))
+      path.replaceWith(expression)
     }
   }
 })
 
-function prepareConfig(options) {
-  const pragmas = {
-    createElement: options.pragma,
-    Fragment: options.pragmaFrag
-  }
-  const {
-    alwaysCreateElement = true,
-    alwaysFragment = false,
-    declaration = 'const'
-  } = options
-  return { alwaysCreateElement, alwaysFragment, declaration, pragmas }
-}
-
 function getDataFromImportNode(node) {
   const { specifiers } = node
 
-  const identifier =
+  const variable =
     t.isImportNamespaceSpecifier(specifiers[0]) ||
     t.isImportDefaultSpecifier(specifiers[0])
       ? specifiers[0].local.name
-      : undefined
+      : null
 
-  const namedSpecifiers = specifiers.filter(s => s.type === 'ImportSpecifier')
+  const namedSpecifiers = specifiers.filter(t.isImportSpecifier)
   const imported = namedSpecifiers.map(s => s.imported.name)
   const locals = namedSpecifiers.map(s => s.local.name)
 
-  return { imported, locals, identifier }
+  return { variable, namedSpecifiers, imported, locals }
+}
+
+function emulateImportSpecifier(name) {
+  return { imported: { name }, local: { name } }
+}
+
+function getAmountOfUse(names, ast) {
+  const amount = {}
+  t.traverse(ast, {
+    enter(path) {
+      // TODO: Rewrite it
+      if (names.includes(path.name) && t.isIdentifier(path)) {
+        const name = path.name
+        amount[name] = (amount[name] || 0) + 1
+      } else if (t.isJSXElement(path)) {
+        amount.createElement = (amount.createElement || 0) + 1
+      } else if (t.isJSXFragment(path)) {
+        amount.createElement = (amount.createElement || 0) + 1
+        amount.Fragment = (amount.Fragment || 0) + 1
+      }
+    }
+  })
+  return amount
+}
+
+function createImportNode(identifer, imports, source) {
+  const specifiers = Object.keys(imports).map(name =>
+    t.importSpecifier(t.identifier(imports[name]), t.identifier(name))
+  )
+
+  return t.importDeclaration(
+    [t.importDefaultSpecifier(identifer), ...specifiers],
+    t.stringLiteral(source)
+  )
+}
+
+function createExtractNode(kind = 'const', identifier, extract) {
+  const properties = Object.keys(extract).map(name =>
+    t.objectProperty(t.identifier(name), t.identifier(extract[name]))
+  )
+
+  return t.variableDeclaration(kind, [
+    t.variableDeclarator(t.objectPattern(properties), identifier)
+  ])
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -3,69 +3,114 @@ const plugin = require('../src')
 
 async function transform(input, options = {}) {
   return await babel.transform(input, {
+    presets: ['@babel/react'],
     plugins: [[plugin(), options]]
   }).code
 }
 
 it('does nothing if source is not `react`', async function() {
   const input = `import {someFunction} from 'some-module'`
-  const output = await transform(input)
-  expect(output).toMatchSnapshot()
+  expect(await transform(input)).toMatchSnapshot()
 })
 
-it('does not extract imports if it is not necessary', async function() {
+it('does not extract imports if there are no imports', async function() {
   const input = `import React from 'react'`
-  const output = await transform(input, { alwaysCreateElement: false })
-  expect(output).toMatchSnapshot()
+  expect(await transform(input)).toMatchSnapshot()
 })
 
-it('extracts React imports to local vars', async function() {
-  const input = `import {Fragment, Suspense} from 'react'`
-  const output = await transform(input)
-  expect(output).toMatchSnapshot()
+it('does not extract imports if they are not used', async function() {
+  const input = `import {useState, useEffect} from 'react'`
+  expect(await transform(input)).toMatchSnapshot()
+})
+
+it('imports properties if they are used once', async function() {
+  const input = `
+    import {useState} from 'react'
+    const [color, setColor] = useState('red')
+  `
+  expect(await transform(input)).toMatchSnapshot()
+})
+
+it('extracts properties if they are used twice or more', async function() {
+  const input = `
+    import {useState} from 'react'
+    const [color, setColor] = useState('red')
+    const [count, setCount] = useState(0)
+  `
+  expect(await transform(input)).toMatchSnapshot()
+})
+
+it('automatically imports `createElement` if JSX is used once', async function() {
+  const input = `
+    import React from 'react'
+    const Header = <h1>Text</h1>
+  `
+  expect(await transform(input)).toMatchSnapshot()
+})
+
+it('automatically extracts `createElement` if JSX is used twice or more', async function() {
+  const input = `
+    import React from 'react'
+    const Header = <h1>Text</h1>
+    const Content = <p>Text</p>
+  `
+  expect(await transform(input)).toMatchSnapshot()
+})
+
+it('automatically imports `Fragment` if it is used', async function() {
+  const input = `
+    import React from 'react'
+    const Content = (
+      <>
+        <h1>Text</h1>
+        <p>Text</p>
+      </>
+    )
+  `
+  expect(await transform(input)).toMatchSnapshot()
+})
+
+it('uses specified local name of imported properties', async function() {
+  const input = `
+    import {useState as state} from 'react'
+    const [color, setColor] = state('red')
+  `
+  expect(await transform(input)).toMatchSnapshot()
+})
+
+it('uses specified local name of extracted properties', async function() {
+  const input = `
+    import {useState as state} from 'react'
+    const [color, setColor] = state('red')
+    const [count, setCount] = state(0)
+  `
+  expect(await transform(input)).toMatchSnapshot()
 })
 
 it('uses specified name of default imported object', async function() {
-  const input = `import SomeName, {Fragment} from 'react'`
-  const output = await transform(input)
-  expect(output).toMatchSnapshot()
+  const input = `
+    import MyReact from 'react'
+    const Header = <h1>Text</h1>
+    const Content = <p>Text</p>
+  `
+  expect(await transform(input)).toMatchSnapshot()
 })
 
 it('transforms namespaced import statement', async function() {
-  const input = `import * as React from 'react'`
-  const output = await transform(input)
-  expect(output).toMatchSnapshot()
-})
-
-it('always imports `createElement` if option is enabled', async function() {
-  const input = `import React from 'react'`
-  const output = await transform(input)
-  expect(output).toMatchSnapshot()
-})
-
-it('does not always import `createElement` if option is disabled', async function() {
-  const input = `import React, {Fragment} from 'react'`
-  const output = await transform(input, { alwaysCreateElement: false })
-  expect(output).toMatchSnapshot()
-})
-
-it('always imports `Fragment` if option is enabled', async function() {
-  const input = `import React from 'react'`
-  const output = await transform(input, { alwaysFragment: true })
-  expect(output).toMatchSnapshot()
-})
-
-it('uses specified `pragma` and `pragmaFrag`', async function() {
-  const input = `import {Fragment} from 'react'`
-  const output = await transform(input, {
-    pragma: 'specifiedCE',
-    pragmaFrag: 'specifiedFragment'
-  })
-  expect(output).toMatchSnapshot()
+  const input = `
+    import * as React from 'react'
+    const Header = <h1>Text</h1>
+    const Content = <p>Text</p>
+  `
+  expect(await transform(input)).toMatchSnapshot()
 })
 
 it('uses specified declaration token and `const` by default', async function() {
-  const input = `import React from 'react'`
+  const input = `
+    import React from 'react'
+    const Header = <h1>Text</h1>
+    const Content = <p>Text</p>
+  `
   const output = await transform(input)
   expect(output).toMatchSnapshot()
   const outputVar = await transform(input, { declaration: 'var' })
