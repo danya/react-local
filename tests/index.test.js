a500689a63e7d80e072a2c317fe2376b0d7054be
Daniil Poroshin <daniilporoshin@gmail.com>
2018-11-17 23:56:42 +0300

    Use sync tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,63 +1,63 @@
 const babel = require('@babel/core')
 const plugin = require('../src')
 
-async function transform(input, options = {}) {
-  return await babel.transform(input, {
+function transform(input, options = {}) {
+  return babel.transformSync(input, {
     presets: ['@babel/react'],
     plugins: [[plugin(), options]]
   }).code
 }
 
-it('does nothing if source is not `react`', async function() {
+it('does nothing if source is not `react`', function() {
   const input = `import {someFunction} from 'some-module'`
-  expect(await transform(input)).toMatchSnapshot()
+  expect(transform(input)).toMatchSnapshot()
 })
 
-it('does not extract imports if there are no imports', async function() {
+it('does not extract imports if there are no imports', function() {
   const input = `import React from 'react'`
-  expect(await transform(input)).toMatchSnapshot()
+  expect(transform(input)).toMatchSnapshot()
 })
 
-it('does not extract imports if they are not used', async function() {
+it('does not extract imports if they are not used', function() {
   const input = `import {useState, useEffect} from 'react'`
-  expect(await transform(input)).toMatchSnapshot()
+  expect(transform(input)).toMatchSnapshot()
 })
 
-it('imports properties if they are used less than specfied', async function() {
+it('imports properties if they are used less than specfied', function() {
   const input = `
     import {useState} from 'react'
     const [color, setColor] = useState('red')
   `
-  expect(await transform(input, { extract: 2 })).toMatchSnapshot()
+  expect(transform(input, { extract: 2 })).toMatchSnapshot()
 })
 
-it('extracts properties if they are used twice or more', async function() {
+it('extracts properties if they are used twice or more', function() {
   const input = `
     import {useState} from 'react'
     const [color, setColor] = useState('red')
     const [count, setCount] = useState(0)
   `
-  expect(await transform(input)).toMatchSnapshot()
+  expect(transform(input)).toMatchSnapshot()
 })
 
-it('automatically imports `createElement` if JSX is used less than specified', async function() {
+it('automatically imports `createElement` if JSX is used less than specified', function() {
   const input = `
     import React from 'react'
     const Header = <h1>Text</h1>
   `
-  expect(await transform(input, { extract: 2 })).toMatchSnapshot()
+  expect(transform(input, { extract: 2 })).toMatchSnapshot()
 })
 
-it('automatically extracts `createElement` if JSX is used', async function() {
+it('automatically extracts `createElement` if JSX is used', function() {
   const input = `
     import React from 'react'
     const Header = <h1>Text</h1>
     const Content = <p>Text</p>
   `
-  expect(await transform(input)).toMatchSnapshot()
+  expect(transform(input)).toMatchSnapshot()
 })
 
-it('automatically imports/extracts `Fragment` if it is used', async function() {
+it('automatically imports/extracts `Fragment` if it is used', function() {
   const input = `
     import React from 'react'
     const Content = (
@@ -67,54 +67,54 @@ it('automatically imports/extracts `Fragment` if it is used', async function() {
       </>
     )
   `
-  expect(await transform(input, { extract: 2 })).toMatchSnapshot()
+  expect(transform(input, { extract: 2 })).toMatchSnapshot()
 })
 
-it('uses specified local name of imported properties', async function() {
+it('uses specified local name of imported properties', function() {
   const input = `
     import {useState as state} from 'react'
     const [color, setColor] = state('red')
   `
-  expect(await transform(input, { extract: 2 })).toMatchSnapshot()
+  expect(transform(input, { extract: 2 })).toMatchSnapshot()
 })
 
-it('uses specified local name of extracted properties', async function() {
+it('uses specified local name of extracted properties', function() {
   const input = `
     import {useState as state} from 'react'
     const [color, setColor] = state('red')
     const [count, setCount] = state(0)
   `
-  expect(await transform(input)).toMatchSnapshot()
+  expect(transform(input)).toMatchSnapshot()
 })
 
-it('uses specified name of default imported object', async function() {
+it('uses specified name of default imported object', function() {
   const input = `
     import MyReact from 'react'
     const Header = <h1>Text</h1>
     const Content = <p>Text</p>
   `
-  expect(await transform(input)).toMatchSnapshot()
+  expect(transform(input)).toMatchSnapshot()
 })
 
-it('transforms namespaced import statement', async function() {
+it('transforms namespaced import statement', function() {
   const input = `
     import * as React from 'react'
     const Header = <h1>Text</h1>
     const Content = <p>Text</p>
   `
-  expect(await transform(input)).toMatchSnapshot()
+  expect(transform(input)).toMatchSnapshot()
 })
 
-it('uses specified declaration token and `const` by default', async function() {
+it('uses specified declaration token and `const` by default', function() {
   const input = `
     import React from 'react'
     const Header = <h1>Text</h1>
     const Content = <p>Text</p>
   `
-  const output = await transform(input)
+  const output = transform(input)
   expect(output).toMatchSnapshot()
-  const outputVar = await transform(input, { declaration: 'var' })
+  const outputVar = transform(input, { declaration: 'var' })
   expect(outputVar).toMatchSnapshot()
-  const outputLet = await transform(input, { declaration: 'let' })
+  const outputLet = transform(input, { declaration: 'let' })
   expect(outputLet).toMatchSnapshot()
 })
