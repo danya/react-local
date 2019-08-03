b98603c4eca8a927c3988d176dc404801db5d2fe
Daniil Poroshin <daniilporoshin@gmail.com>
2019-08-04 01:41:09 +0300

    Update tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,138 +1,51 @@
 const babel = require('@babel/core')
 const plugin = require('../src')
 
-function transform(input, options = {}) {
+function transform(input) {
   return babel.transformSync(input, {
     presets: ['@babel/react'],
-    plugins: [[plugin(), options]]
+    plugins: [plugin()]
   }).code
 }
 
-it('does nothing if source is not `react`', function() {
-  const input = `import {something} from 'some-where'`
-  expect(transform(input)).toMatchSnapshot()
-})
-
-it('does nothing if there are no named improts from `react`', function() {
-  const input = `import React from 'react'`
-  expect(transform(input)).toMatchSnapshot()
-})
-
-it('does not extract imports if they are not used', function() {
-  const input = `import {useState, useEffect} from 'react'`
-  expect(transform(input)).toMatchSnapshot()
-})
-
-it('transforms namespaced import statement', function() {
+it('creates local variables for named imports', () => {
   const input = `
-    import * as React from 'react'
-    const element = <div></div>
+    import React, {useState, useEffect} from 'react';
   `
-  expect(transform(input)).toMatchSnapshot()
+  const output = transform(input)
+  expect(output).toMatchSnapshot()
 })
 
-it('imports properties if they are used less than specfied', function() {
+it('supports import aliasing', () => {
   const input = `
-    import {useState, useEffect} from 'react'
-    const state = useState(0)
-    useEffect(() => {})
+    import React, {useState as foo} from 'react';
   `
-  expect(transform(input, { extract: 2 })).toMatchSnapshot()
+  const output = transform(input)
+  expect(output).toMatchSnapshot()
 })
 
-it('extracts properties if they are used more than specified (defaults to 0)', function() {
+it('supports import with no default import', () => {
   const input = `
-    import {useState} from 'react'
-    const state1 = useState(0)
-    const state2 = useState(0)
+    import {useState} from 'react';
   `
-  expect(transform(input)).toMatchSnapshot()
+  const output = transform(input)
+  expect(output).toMatchSnapshot()
 })
 
-it('uses named imports and extract together', function() {
+it('supports mixed imports', () => {
   const input = `
-    import {useState, useEffect} from 'react'
-    const state = useState(0)
-    useEffect(() => {})
-    useEffect(() => {})
+    import React, {useEffect} from 'react'
+    import {useState} from 'react';
   `
-  expect(transform(input, { extract: 2 })).toMatchSnapshot()
+  const output = transform(input)
+  expect(output).toMatchSnapshot()
 })
 
-it('imports/extracts properties used as JSX elements', function() {
+it('transforms React.createElement calls', () => {
   const input = `
-    import {Suspense} from 'react'
-    const element = <Suspense></Suspense>
+    import React from 'react';
+    const element = React.createElement('div');
   `
-  expect(transform(input)).toMatchSnapshot()
-})
-
-it('automatically imports/extracts `createElement` if JSX is used', function() {
-  const input = `
-    import React from 'react'
-    const element = <div></div>
-  `
-  expect(transform(input)).toMatchSnapshot()
-})
-
-it('automatically imports/extracts `Fragment` if it is used', function() {
-  const input = `
-    import React from 'react'
-    const fragment = <></>
-  `
-  expect(transform(input)).toMatchSnapshot()
-})
-
-it('injects `createElement` and `Fragment` only once per file', function() {
-  const input = `
-    import * as React from 'react'
-    import {useState} from 'react'
-    const fragment = <></>
-    const element = <div></div>
-    const state = useState(0)
-  `
-  expect(transform(input)).toMatchSnapshot()
-})
-
-it('uses specified local name of imported properties', function() {
-  const input = `
-    import {useState as createState} from 'react'
-    const state = createState(0)
-  `
-  expect(transform(input, { extract: 2 })).toMatchSnapshot()
-})
-
-it('uses specified local name of extracted properties', function() {
-  const input = `
-    import {useState as createState} from 'react'
-    const state = createState(0)
-  `
-  expect(transform(input)).toMatchSnapshot()
-})
-
-it('uses specified name of default imported object', function() {
-  const input = `
-    import MyReact from 'react'
-    const element = <div></div>
-  `
-  expect(transform(input)).toMatchSnapshot()
-})
-
-it('generates unique name in scope for default imported object', function() {
-  const input = `
-    import {useState} from 'react'
-    const _React = 1;
-    const state = useState(0)
-  `
-  expect(transform(input)).toMatchSnapshot()
-})
-
-it('uses specified declaration token and `const` by default', function() {
-  const input = `
-    import React from 'react'
-    const element = <div></div>
-  `
-  expect(transform(input)).toMatchSnapshot()
-  expect(transform(input, { declaration: 'var' })).toMatchSnapshot()
-  expect(transform(input, { declaration: 'let' })).toMatchSnapshot()
+  const output = transform(input)
+  expect(output).toMatchSnapshot()
 })
