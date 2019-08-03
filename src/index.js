43c875d93c2f9db5564ed4e5cf1efff04679c11e
Daniil Poroshin <daniilporoshin@gmail.com>
2019-08-04 01:40:27 +0300

    Update code

--- src/index.js
+++ src/index.js
@@ -1,143 +1,130 @@
-const {types: t, traverse} = require('@babel/core');
+const { types: t } = require('@babel/core')
 
-const VISITED = Symbol();
-const CREATE_ELEMENT = Symbol();
-const FRAGMENT = Symbol();
-
-module.exports = () => ({
-  name: 'babel-plugin-react-local',
-
-  pre() {
-    this.autoimport = new Map();
-  },
-
-  visitor: {
-    ImportDeclaration(path, {opts, filename}) {
-      if (path.node.source.value !== 'react' || path.node[VISITED]) return;
-
-      const options = {declaration: 'const', extract: 'all', ...opts};
-
-      const {variable, namedSpecifiers, locals} = getDataFromImportNode(
-        path.node
-      );
-      const specifiers = [...namedSpecifiers];
-      const usageCount = getUsageCount(locals, path.parent);
-      const extractCount = options.extract === 'all' ? 0 : options.extract;
-      const imports = {};
-      const extract = {};
-
-      if (!this.autoimport.has(filename)) {
-        const autoimport = {};
-        [CREATE_ELEMENT, FRAGMENT].forEach(name => {
-          if (usageCount[name] === 0) return;
-          const importedName =
-            name === CREATE_ELEMENT ? 'createElement' : 'Fragment';
-          const localName = path.scope.generateUidIdentifier(importedName);
-
-          specifiers.push(
-            t.importSpecifier(localName, t.identifier(importedName))
-          );
-          usageCount[localName.name] = usageCount[name];
-          autoimport[importedName] = localName;
-        });
-        this.autoimport.set(filename, autoimport);
-      }
-
-      for (let i = 0, l = specifiers.length; i < l; i++) {
-        const importedName = specifiers[i].imported.name;
-        const localName = specifiers[i].local.name;
-        const count = usageCount[localName];
-        if (count >= extractCount) {
-          extract[localName] = importedName;
-        } else if (count > 0) {
-          imports[localName] = importedName;
-        }
-      }
-
-      const identifier = variable
-        ? t.identifier(variable)
-        : path.scope.generateUidIdentifier('React');
-      const importNode = createImportNode(identifier, imports, 'react');
-      importNode[VISITED] = true;
-      const extractNode =
-        Object.keys(extract).length > 0
-          ? createExtractNode(options.declaration, identifier, extract)
-          : null;
-
-      path.replaceWithMultiple([importNode, extractNode].filter(Boolean));
-    },
-
-    MemberExpression(path, {filename}) {
-      if (
-        path.node.object.name !== 'React' ||
-        t.isVariableDeclarator(path.parent) ||
-        (path.node.property.name !== 'createElement' &&
-          path.node.property.name !== 'Fragment')
+module.exports = function() {
+  // Transform named imports to local variables
+  function visitImportDeclaration(path, state) {
+    if (path.node.source.value !== 'react') {
+      return
+    }
+    // Collect named specifiers to list and remove them from the import
+    // statement. Default and namespace specifiers are saved
+    const imports = []
+    const specifiers = path.get('specifiers')
+    for (const specifier of specifiers) {
+      if (t.isImportSpecifier(specifier)) {
+        imports.push({
+          imported: specifier.node.imported.name,
+          local: specifier.node.local.name
+        })
+        specifier.remove()
+      } else if (
+        t.isImportDefaultSpecifier(specifier) ||
+        t.isImportNamespaceSpecifier(specifier)
       ) {
-        return;
-      }
-
-      const localName = this.autoimport.get(filename)[path.node.property.name];
-      if (localName) {
-        const expression = t.expressionStatement(localName);
-        path.replaceWith(expression);
-      }
-    },
-  },
-});
-
-function getDataFromImportNode(node) {
-  const {specifiers} = node;
-  const variable =
-    t.isImportNamespaceSpecifier(specifiers[0]) ||
-    t.isImportDefaultSpecifier(specifiers[0])
-      ? specifiers[0].local.name
-      : null;
-  const namedSpecifiers = specifiers.filter(t.isImportSpecifier);
-  const locals = namedSpecifiers.map(s => s.local.name);
-
-  return {variable, namedSpecifiers, locals};
-}
-
-function getUsageCount(names, ast) {
-  const amount = {};
-  const inc = prop => (amount[prop] = (amount[prop] || 0) + 1);
-  traverse(ast, {
-    enter(path) {
-      if (path.isImportDeclaration() && path.node.source.value === 'react') {
-        path.skip();
-      } else if (names.includes(path.node.name) && path.isIdentifier()) {
-        inc(path.node.name);
-      } else if (path.isJSXOpeningElement()) {
-        inc(CREATE_ELEMENT);
-        const name = path.node.name.name;
-        if (names.includes(name)) {
-          inc(name);
+        const local = specifier.get('local')
+        // We need default or namespace import bounded to local
+        // variable `React` because this name is used as default
+        // pragma in Babel's JSX compiler
+        if (local.node.name === 'React') {
+          state.generalImport = path
         }
-      } else if (path.isJSXFragment()) {
-        inc(CREATE_ELEMENT);
-        inc(FRAGMENT);
       }
-    },
-  });
-  return amount;
-}
+    }
+    // We need default or namespace import from React for
+    // variable declaration, so if there is no default or
+    // namespace import just add it
+    if (!state.generalImport) {
+      const defaultSpecifier = t.importDefaultSpecifier(t.identifier('React'))
+      path.pushContainer('specifiers', [defaultSpecifier])
+      path.scope.registerDeclaration(path)
+      state.generalImport = path
+    }
+    // In case we have multiple imports we can just remove the last
+    // one because we can use default import from the first statement
+    const updatedSpecifiers = path.get('specifiers')
+    if (updatedSpecifiers.length === 0) {
+      path.remove()
+    }
+    // Create a destructuring variable declaration
+    if (imports.length > 0) {
+      const declarator = t.variableDeclarator(
+        t.objectPattern(
+          imports.map(specifier =>
+            t.objectProperty(
+              t.identifier(specifier.imported),
+              t.identifier(specifier.local),
+              false,
+              specifier.imported === specifier.local
+            )
+          )
+        ),
+        t.identifier('React')
+      )
+      // Every time insert variable declaration right after the first
+      // import statement with default or namespace specifiers bounded to `React`
+      const importPath = state.generalImport
+      importPath.insertAfter(t.variableDeclaration('const', [declarator]))
+      importPath.scope.registerDeclaration(path.getNextSibling())
+    }
+  }
 
-function createImportNode(identifier, imports, source) {
-  const specifiers = [
-    t.importDefaultSpecifier(identifier),
-    ...Object.keys(imports).map(name =>
-      t.importSpecifier(t.identifier(name), t.identifier(imports[name]))
-    ),
-  ];
-  return t.importDeclaration(specifiers, t.stringLiteral(source));
-}
+  // Check if object in member expression is reference to React
+  function isReactImport(path) {
+    const identifierName = path.node.name
+    const binding = path.scope.getBinding(identifierName)
+    if (
+      t.isImportDefaultSpecifier(binding.path) ||
+      t.isImportNamespaceSpecifier(binding.path)
+    ) {
+      const parentPath = binding.path.parentPath
+      return (
+        t.isImportDeclaration(parentPath) &&
+        parentPath.node.source.value === 'react'
+      )
+    }
+    return false
+  }
 
-function createExtractNode(kind, identifier, extract) {
-  const id = t.objectPattern(
-    Object.keys(extract).map(name =>
-      t.objectProperty(t.identifier(extract[name]), t.identifier(name))
-    )
-  );
-  return t.variableDeclaration(kind, [t.variableDeclarator(id, identifier)]);
+  // Insert after import statement reference to `React.createElement`
+  // and return its name for use across all AST
+  function createCreateElementRef(importPath) {
+    const reference = importPath.scope.generateUidIdentifier('createElement')
+    const declaration = t.variableDeclaration('const', [
+      t.variableDeclarator(
+        reference,
+        t.memberExpression(t.identifier('React'), t.identifier('createElement'))
+      )
+    ])
+    importPath.insertAfter(declaration)
+    importPath.scope.registerDeclaration(importPath.getNextSibling())
+    return reference
+  }
+
+  // Transform each calling of `React.createElement` to calling of its
+  // alias declared in a global scope (import statement scope)
+  function visitCallExpression(path, state) {
+    const callee = path.get('callee')
+    if (t.isMemberExpression(callee)) {
+      const object = callee.get('object')
+      if (!isReactImport(object)) {
+        return
+      }
+      const property = callee.get('property')
+      if (t.isIdentifier(property) && property.node.name === 'createElement') {
+        // We keep it in state to be sure we avoid name collision in each file
+        if (!state.createElementRef) {
+          state.createElementRef = createCreateElementRef(state.generalImport)
+        }
+        callee.replaceWith(state.createElementRef)
+      }
+    }
+  }
+
+  return {
+    name: 'babel-plugin-react-local',
+    visitor: {
+      ImportDeclaration: visitImportDeclaration,
+      CallExpression: visitCallExpression
+    }
+  }
 }
